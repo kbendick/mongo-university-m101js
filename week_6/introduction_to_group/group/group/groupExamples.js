fc5871c61ba3df60f2c9b070e8490ef520d44d72
Kyle Bendickson <kbendick@uci.edu>
2016-02-15 14:19:24 -0800

    added notes on

--- week_6/introduction_to_group/group/group/groupExamples.js
+++ week_6/introduction_to_group/group/group/groupExamples.js
@@ -1,3 +1,17 @@
+// $group - similar to SQL GROUP BY command
+//
+//      - in group stage, can aggregate together values in 
+//         multiple documents
+//
+// _id field is fundamental to the group stage.
+//    - how we define / control / tune how the group
+//      stages organizes the documents into groups.
+
+
+
+//    - in this case, $avg will calculate an average
+//      for # of employees for all documents grouped by
+//      founded year.
 db.companies.aggregate([
     { $group: {
         _id: { founded_year: "$founded_year" },
@@ -7,6 +21,8 @@ db.companies.aggregate([
     
 ])
 
+
+// doesnt use $group, we were just further exploring the data
 db.companies.aggregate( [
     { $match : { founded_year : 2001 } },
     { $project : { _id: 0, name : 1, number_of_employees: 1 } },
@@ -14,6 +30,7 @@ db.companies.aggregate( [
 ] )
 
 
+// match on relationships.person.
 db.companies.aggregate( [
     { $match: { "relationships.person": { $ne: null } } },
     { $project: { relationships: 1, _id: 0 } },
