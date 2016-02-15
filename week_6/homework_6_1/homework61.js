ee6020969a26d057d35f805db310416819006930
Kyle Bendickson <kbendick@uci.edu>
2016-02-15 14:36:48 -0800

    added homework6.1 solution

---
+++ week_6/homework_6_1/homework61.js
@@ -0,0 +1,21 @@
+
+// Prompt: Write an aggregation query that will determine the number
+//         of unique companies with which an individual has been associated.
+db.companies.aggregate( [
+    { $match: { "relationships.person": { $ne: null } } },
+    { $project: { 
+    	relationships: 1,
+    	company_name: "$name",
+    	_id: 0 
+    } },
+    { $unwind: "$relationships" },
+    { $group: {
+        _id: "$relationships.person",
+        unique_companies: {$addToSet: "$company_name"}
+    } },
+    { $project: {
+        _id: 1,
+        unique_companies_count: {$size: "$unique_companies"}
+    } },
+    { $sort: { unique_companies_count: -1 } }
+] )
